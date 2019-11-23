1c4b026db1ed5daad0fb6c0f96f383548393a6df
Neil Obremski <neilobremski@BLVLTM003.local>
2019-11-23 11:28:14 -0800

    Added mssql.close()

--- checksum-dump.js
+++ checksum-dump.js
@@ -4,13 +4,13 @@ const mssql = require('mssql');
     console.log("Started");
     try {
         await mssql.connect(process.env.CONNECTION_STRING);
-        const result = await mssql.query`SELECT CHECKSUM('a') AS result`;
+        const result = await mssql.query`SELECT CHECKSUM(CONCAT(CHAR(48), CHAR(48))) AS result`;
         console.dir(result);
+        await mssql.close();
         console.log("Success!");
     } catch (err) {
         console.log(err);
         console.log("Failed!");
     }
     console.log("Finished");
-    process.exit(0);
 })();
